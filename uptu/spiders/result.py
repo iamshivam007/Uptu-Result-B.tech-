02b0ce23e92f61c73458f47ae968c4f5b69627d6
shivji <jindalshivam65@gmail.com>
2016-03-25 17:26:28 +0530

    initial commit

---
+++ uptu/spiders/result.py
@@ -0,0 +1,163 @@
+import os
+import scrapy
+from selenium.webdriver.common.action_chains import ActionChains
+from scrapy import signals
+from scrapy.xlib.pydispatch import dispatcher
+from scrapy.http import TextResponse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+import xlwt
+from Tkinter import *
+
+class Result(scrapy.Spider):
+    name = "result"
+    roll = 1409110901
+    sheet = None
+    workbook = None
+    count = 1
+    allowed_domains = ['http://new.aktu.co.in/']
+    start_urls = ['http://new.aktu.co.in/']
+
+    def __init__(self, filename=None):
+    	self.workbook = xlwt.Workbook()
+        self.sheet = self.workbook.add_sheet('Sheet_1')
+        self.sheet.write(0,0,'Name')
+        self.sheet.write(0,1,"Father's Name")
+        self.sheet.write(0,2,'Roll No.')
+        self.sheet.write(0,3,'Enrollment No.')
+        self.sheet.write(0,4,'Branch')
+        self.sheet.write(0,5,'College')
+        self.sheet.write(0,6,item['s1'])
+        self.sheet.write(0,7,item['s2'])
+        self.sheet.write(0,8,item['s3'])
+        self.sheet.write(0,9,item['s4'])
+        self.sheet.write(0,10,item['s5'])
+        self.sheet.write(0,11,item['s6'])
+        self.sheet.write(0,12,'GP')
+        self.sheet.write(0,13,'Total')
+        self.driver = webdriver.Firefox()
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
+
+    def spider_closed(self, spider):
+        self.workbook.save('result.xls')
+
+
+    def add_in_sheet(self,item):
+        self.count += 1
+        self.sheet.write(self.count,0,item['name'])
+        self.sheet.write(self.count,1,item['father'])
+        self.sheet.write(self.count,2,item['roll'])
+        self.sheet.write(self.count,3,item['enroll'])
+        self.sheet.write(self.count,4,item['branch'])
+        self.sheet.write(self.count,5,item['clg'])
+        self.sheet.write(self.count,6,item[item['s1']])
+        self.sheet.write(self.count,7,item[item['s2']])
+        self.sheet.write(self.count,8,item[item['s3']])
+        self.sheet.write(self.count,9,item[item['s4']])
+        self.sheet.write(self.count,10,item[item['s5']])
+        self.sheet.write(self.count,11,item[item['s6']])
+        self.sheet.write(self.count,12,item['gp'])
+        self.sheet.write(self.count,13,item['tot'])
+        
+
+    def parse_result(self, response):
+        item = {}
+        # Load the current page into Selenium
+        
+        self.driver.get(response.url)
+        try:
+            WebDriverWait(self.driver, 40).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_imgstud"]')))
+        except TimeoutException:
+            print "Time out"
+            return
+        # Sync scrapy and selenium so they agree on the page we're looking at then let scrapy take over
+        resp = TextResponse(url=self.driver.current_url, body=self.driver.page_source, encoding='utf-8');
+        temp = format(resp.xpath('//*[@id="lblname"]/text()').extract())
+        item['name'] = temp[3:-2]
+
+        temp = format(resp.xpath('//*[@id="lblfname"]/text()').extract())
+        item['father'] = temp[3:-2]
+
+        temp = format(resp.xpath('//*[@id="lblrollno"]/text()').extract())
+        item['roll'] = temp[3:-2]
+
+        temp = format(resp.xpath('//*[@id="lblenrollno"]/text()').extract())
+        item['enroll'] = temp[3:-2]
+
+        temp = format(resp.xpath('//*[@id="lblbranch"]/text()').extract())
+        item['branch'] = temp[3:-2]
+
+        temp = format(resp.xpath('//*[@id="lblcollegename"]/text()').extract())
+        item['clg'] = temp[3:-2]
+
+        temp = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td[2]/b/text()').extract())
+        item['s1'] = temp[3:-2]
+
+        t1 = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td[3]/b/text()').extract())
+        t2  = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td[4]/b/text()').extract())
+        item[item['s1']] = t1[3:-2] + ' , '  + t2[3:-2]
+
+        temp = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/b/text()').extract())
+        item['s2'] = temp[3:-2]
+        t1 = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td[3]/b/text()').extract())
+        t2  = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td[4]/b/text()').extract())
+        item[item['s2']] = t1[3:-2] + ' , '  + t2[3:-2]
+
+
+        temp = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[4]/td[2]/b/text()').extract())
+        item['s3'] = temp[3:-2]
+        t1 = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[4]/td[3]/b/text()').extract())
+        t2  = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[4]/td[4]/b/text()').extract())
+        item[item['s3']] = t1[3:-2] + ' , '  + t2[3:-2]    
+
+        temp = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[5]/td[2]/b/text()').extract())
+        item['s4'] = temp[3:-2]
+        t1 = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[5]/td[3]/b/text()').extract())
+        t2  = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[5]/td[4]/b/text()').extract())
+        item[item['s4']] = t1[3:-2] + ' , '  + t2[3:-2]
+
+        temp = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[6]/td[2]/b/text()').extract())
+        item['s5'] = temp[3:-2]
+        t1 = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[6]/td[3]/b/text()').extract())
+        t2  = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[6]/td[4]/b/text()').extract())
+        item[item['s5']] = t1[3:-2] + ' , '  + t2[3:-2]
+
+        temp = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[7]/td[2]/b/text()').extract())
+        item['s6'] = temp[3:-2]
+        t1 = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[7]/td[3]/b/text()').extract())
+        t2  = format(resp.xpath('//*[@id="Pane0_content"]/table[1]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[7]/td[4]/b/text()').extract())
+        item[item['s6']] = t1[3:-2] + ' , '  + t2[3:-2]
+
+        temp = format(resp.xpath('//*[@id="ctl00_ContentPlaceHolder1_tr1"]/td[3]/text()').extract())
+        item['gp'] = temp[3:-2]
+        temp = format(resp.xpath('//*[@id="Pane0_content"]/table[3]/tbody/tr[2]/td[3]/text()').extract())
+        print temp
+        item['tot'] = temp[3:-2]
+        self.add_in_sheet(item)
+
+    def parse(self, response):
+        for i in xrange(20):
+            self.driver.get('http://new.aktu.co.in/')
+            try:
+                WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.XPATH,'//*[@id="ctl00_ContentPlaceHolder1_divSearchRes"]/center/table/tbody/tr[4]/td/center/div/div/img')))
+            except:
+                yield scrapy.Request(url="http://new.aktu.co.in/",callback=self.parse)
+	        # Sync scrapy and selenium so they agree on the page we're looking at then let scrapy take over
+            resp = TextResponse(url=self.driver.current_url, body=self.driver.page_source, encoding='utf-8');
+            rollno = self.driver.find_element_by_name('ctl00$ContentPlaceHolder1$TextBox1')
+            rollno.send_keys(self.roll)
+            submit = self.driver.find_element_by_name('ctl00$ContentPlaceHolder1$btnSubmit')
+            actions = ActionChains(self.driver)
+            time.sleep(5)
+            actions.click(submit)
+            actions.perform()
+            time.sleep(5)
+            resp = TextResponse(url=self.driver.current_url, body=self.driver.page_source, encoding='utf-8');
+            self.parse_result(resp)
+            self.roll += 1
+            return
+
